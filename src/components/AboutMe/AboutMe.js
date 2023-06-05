95113f023a68136ef0454dadd4ebd68d9fbe62fa
Kalina Frontend <dzhiltcov@yandex.ru>
2023-06-06 00:48:02 +0200

    feat: добавить aboutMe элемент

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./AboutMe.css";
+import foto from '../../images/about-me__foto.jpg';
+import arrow from "../../images/about-me__link-arrow.svg";
+
+function AboutMe() {
+  return (
+    <section className='about-me'>
+      <a name='about-me'></a>
+      <h3 className='about-me__title'>Студент</h3>
+      <div className='about-me__container'>
+        <div className='about-me__description'>
+          <h4 className='about-me__name'>Дмитрий</h4>
+          <p className='about-me__about'>Фронтенд-разработчик, 33 года</p>
+          <p className='about-me__biography'>Я родился в Нижнем Новгороде, сейчас живу в Калининграде. В 2009 окончил колледж по направлению автоматизированная система управления (АСУ). У меня есть жена 
+            и дочь. Я люблю слушать музыку, и читать книги. Код пишу давно но сейчас решил серьёзно заняться именно Фронтендом. 
+            С 2011 года работаю системным администратором крупной сети. После того, как прошёл курс по веб-разработке, 
+            начал заниматься фриланс-заказами, сейчас занят поиском работы но веб-разработке.</p>
+          <p className='about-me__github-link-container'>
+            <a className='about-me__github-link' href='https://github.com/KalinaFrontend' target="_blank">
+              Github
+            </a>
+          </p>
+        </div>
+        <img className='about-me__foto' src={foto} alt='фотография студента' />
+      </div>
+      <p className='about-me__portfolio-link'>Портфолио</p>
+      <ul className='about-me__navigation'>
+        <li>
+          <a className='about-me__link-container' href='https://kalinafrontend.github.io/How-to-learn/' target="_blank">
+            <p className='about-me__link-name'>Статичный сайт</p>
+            <img className='about-me__link-arrow' src={arrow} alt='стрелка'/>
+          </a>
+        </li>
+        <li>
+          <a className='about-me__link-container' href='https://kalinafrontend.github.io/Travel-in-Russia/' target="_blank">
+            <p className='about-me__link-name'>Адаптивный сайт</p>
+            <img className='about-me__link-arrow' src={arrow} alt='стрелка'/>
+          </a>
+        </li>
+        <li>
+          <a className='about-me__link-container' href='https://mesto-react-kalina.nomoredomains.monster' target="_blank">
+            <p className='about-me__link-name'>Одностраничное приложение</p>
+            <img className='about-me__link-arrow' src={arrow} alt='стрелка'/>
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default AboutMe;
---
+++ src/components/Techs/Techs.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Techs.css";
+
+function Techs() {
+  return (
+    <section className='technology'>
+      <h3 className='technology__title'>Технологии</h3>
+      <h4 className='technology__description-title'>7 технологий</h4>
+      <p className='technology__description'>На курсе веб-разработки мы освоили технологии,
+        которые применили  <br/> в дипломном проекте.</p>
+      <nav className='technology__navigation'>
+        <a className='technology__link' href='https://ru.wikipedia.org/wiki/HTML' target="_blank" rel="noreferrer">
+          <button className='technology__button' type='button'>HTML</button>
+        </a>
+        <a className='technology__link' href='https://ru.wikipedia.org/wiki/CSS' target="_blank" rel="noreferrer">
+          <button className='technology__button' type='button'>CSS</button>
+        </a>
+        <a className='technology__link' href='https://www.javascript.com/' target="_blank" rel="noreferrer">
+          <button className='technology__button' type='button'>JS</button>
+        </a>
+        <a className='technology__link' href='https://reactjs.org/' target="_blank" rel="noreferrer">
+          <button className='technology__button' type='button'>React</button>
+        </a>
+        <a className='technology__link' href='https://git-scm.com/' target="_blank" rel="noreferrer">
+          <button className='technology__button' type='button'>Git</button>
+        </a>
+        <a className='technology__link' href='https://expressjs.com/ru/' target="_blank" rel="noreferrer">
+          <button className='technology__button' type='button'>Express.js</button>
+        </a>
+        <a className='technology__link' href='https://www.mongodb.com/' target="_blank" rel="noreferrer">
+          <button className='technology__button' type='button'>mongoDB</button>
+        </a>
+      </nav>
+    </section>
+  );
+}
+
+export default Techs;
